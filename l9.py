862bcd5e30f754040f9270154e06a0a33c21bb7e
Nikita <123164416+hekaruowo@users.noreply.github.com>
2025-06-14 14:14:48 +0300

    52

---
+++ l9.py
@@ -0,0 +1,22 @@
+
+st1 = "Я аавыафывыфвыфвфвыфвфвыфвфdsadadsadsdвыфвфыа"
+
+# [начало : конец : шаг]
+# print(st1[0 : 5 : 2])
+# st2 = st1[::2]
+# lastIndex = st1[-1]
+# print(lastIndex)
+#
+# str3 = ("привет")
+# str4 = ("Негр")
+# print(str3 + str4)
+
+# методы
+# метод count()
+strP = "12321541243213211"
+print(strP.count("1"))
+
+# find rfind index rindex
+print(strP.rindex("1"))
+
+# 
